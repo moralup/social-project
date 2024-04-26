c45cdd7f83c9b6975edf38d8ad2ba059d314a972
Kirill Churichev <absolutemoraldown@gmail.com>
2024-04-26 23:11:24 +0700

    add jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,36 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('class')).toBe('class');
+    });
+
+    test('with additional class', () => {
+        expect(classNames('class', {}, ['class1', 'class2']))
+            .toBe('class class1 class2');
+    });
+
+    test('with mods', () => {
+        expect(classNames('class', { class1: true, class2: true }))
+            .toBe('class class1 class2');
+    });
+
+    test('with mods false', () => {
+        expect(classNames('class', { class1: true, class2: false }))
+            .toBe('class class1');
+    });
+
+    test('with mods undefined', () => {
+        expect(classNames('class', { class1: undefined, class2: true }))
+            .toBe('class class2');
+    });
+
+    test('full param', () => {
+        expect(classNames('class', {
+            class1: undefined,
+            class2: true,
+            class3: false,
+        }, ['class4', 'class5']))
+            .toBe('class class4 class5 class2');
+    });
+});
