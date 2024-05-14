f9867fafd3b995ba0448f6bcb85f202c2aa89263
Kirill Churichev <absolutemoraldown@gmail.com>
2024-05-14 20:33:04 +0700

    fix the path of stub component for svg

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -30,7 +30,7 @@ export default {
     setupFilesAfterEnv: ['<rootDir>/config/jest/setupTests.ts'],
     moduleNameMapper: {
         '\\.s?css': 'identity-obj-proxy',
-        '\\.svg': path.resolve(__dirname, 'JestEmptyComponent.tsx'),
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
