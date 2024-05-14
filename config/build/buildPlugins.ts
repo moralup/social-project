2f6c0848be6fc3d22e88c5f52a8114be1de0cf89
Kirill Churichev <absolutemoraldown@gmail.com>
2024-05-14 20:08:29 +0700

    fix error

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -19,7 +19,7 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         }),
     ];
 
-    if (__IS_DEV__) {
+    if (isDev) {
         plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
         plugins.push(new webpack.HotModuleReplacementPlugin());
     }
